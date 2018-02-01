e0d7aaabae6ee913e561c24ab79bcba2eefcc102
EvaFro <frolli.erin@gmail.com>
2018-02-01 15:57:17 -0800

    Create a working ajax call- function works in console
    Need to hook the wires up to app correctly

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,34 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: $.extend({
+      q: options.query,
+      key: options.key,
+      part: 'snippet'}, {maxResults: options.max}),
+    contentType: 'application/jason',
+    success: function(data) {
+      console.log('Search request retrieved');
+      console.log(data);
+      /*return callback(data); ???*/
+    
+    },
+    error: function(data) {
+      console.log('Bad request!!!!!!');
+    }
+    
+  });
+  
 };
+
+
+
+
+
+
+
+
 
 window.searchYouTube = searchYouTube;
