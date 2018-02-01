b51bca36f18e0a850670217b089b11a567b5dee5
EvaFro <frolli.erin@gmail.com>
2018-01-31 19:53:57 -0800

    Insert data when app is rendered
    Top down data flow

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
 // TODO: Render the `App` component to the DOM
-
-
-/*class AppMaker extends React.Component {
-  constructor() {
-  
-  }  
-
-  render() {
-  }
-
-}*/
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App allVideos={window.exampleVideoData}/>, document.getElementById('app'));
