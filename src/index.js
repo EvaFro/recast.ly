a20959c4900aeb393c5eb370daae99750bf0c9ea
EvaFro <frolli.erin@gmail.com>
2018-01-31 17:23:05 -0800

    Render the app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 // TODO: Render the `App` component to the DOM
 
+
+/*class AppMaker extends React.Component {
+  constructor() {
+  
+  }  
+
+  render() {
+  }
+
+}*/
+
+ReactDOM.render(<App />, document.getElementById('app'));
