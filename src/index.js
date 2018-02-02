e1e7a5fa2067ca89fdb5863361210faffe38830b
EvaFro <frolli.erin@gmail.com>
2018-02-01 20:35:16 -0800

    Revert to state- removed search function from here
    Search is windows so global

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-// TODO: Render the `App` component to the DOM
+// // TODO: Render the `App` component to the DOM
 ReactDOM.render(<App allVideos={window.exampleVideoData}/>, document.getElementById('app'));
